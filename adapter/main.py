dff5433c75f7bd09458824534f5ad2d2a11b373b
elenacalcan26 <elena.calcan26@gmail.com>
2023-01-05 18:28:51 +0200

    setup influxdb container

--- adapter/main.py
+++ adapter/main.py
@@ -1,12 +1,21 @@
 import paho.mqtt.client as mqtt
+from influxdb import InfluxDBClient
 import time
+import json
+
+influxClient = InfluxDBClient('influxdb', 8086)
 
 def on_connect(client, userdata, flags, rc):
     print('Connected with the result code ' + str(rc), flush=True)
     client.subscribe('#')
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload), flush=True)
+    # print(msg.topic+" "+str(msg.payload), flush=True)
+    topic = msg.topic
+    payload = json.loads(msg.payload)
+    print(f'Received a message by topic [{topic}]', flush=True)
+
+    pass
 
 
 def main():
@@ -17,7 +26,7 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect("mqtt", 1883)
+    client.connect('mqtt', 1883)
     client.loop_forever()
 
 if __name__ == '__main__':
